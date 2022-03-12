4979e87ec2d294c9118cc144bff738c5bcb6cd95
lugd0n <daniel.luge@hotmail.com>
2022-03-12 08:50:45 +0100

    project creation

---
+++ src/main/java/com/example/demo/SpendingTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendingTrackerServiceApplication.class, args);
+	}
+
+}
